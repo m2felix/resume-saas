eafb3f5342f69ed28672941cd87e160ba49481ab
m1felix <mike.w.felix@gmail.com>
2025-08-20 23:28:25 -0700

    .

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
